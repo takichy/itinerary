763eb8164909d292a2c91129b282012f3cef592b
takichy <abdelhayoulidiomali@gmail.com>
2020-02-28 04:55:07 +0100

    final test

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title> trace itinéraire - Cirkwi</title>
+    </head>
+    <body>
+        <ol>
+            <li>
+                <a href="./src/index_using_ol.html" target="_blank"> Exemple avec OpenLayer3.</a>
+            </li>
+            <li>
+                <a href="./src/index_gmaps.html" target="_blank">Exemple avec GOOGLE MAPS.</a>
+            </li>
+        </ol>
+    </body>
+</html>
+ 
